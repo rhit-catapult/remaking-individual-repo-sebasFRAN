514ae1786c1ace2508c467af3c509bcb3931c221
sebastianfrancisco <franciscosebastian160@gmail.com>
2025-06-04 15:25:55 -0400

    testing doing a commit

--- 00-IntroToPython/guess_my_word.py
+++ 00-IntroToPython/guess_my_word.py
@@ -1,3 +1,7 @@
 print("Guess My Word")
+
+#Randomly select a secret word
 secret_word = "dog"
 print(secret_word)
+
+#Get the str length of secret_word and make a display_word of *******'s the same length
