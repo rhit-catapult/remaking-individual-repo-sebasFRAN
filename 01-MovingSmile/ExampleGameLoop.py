a18c3d6f9ec0399f1480c2ee1d8cab3a528b6fb8
sebastianfrancisco <franciscosebastian160@gmail.com>
2025-06-05 10:01:56 -0400

    circle

--- 01-MovingSmile/ExampleGameLoop.py
+++ 01-MovingSmile/ExampleGameLoop.py
@@ -1,5 +1,4 @@
-# The two lines below allow you to use PyGame and System functions.
-# Often programmers use code that other developers have written.
+
 import pygame
 import sys
 
@@ -7,20 +6,13 @@ import sys
 pygame.init()
 
 # Let's create a caption for the game window
-pygame.display.set_caption("Hello World")
+pygame.display.set_caption("Sebastian Francisco")
 # TODO 00: Change the window caption to say your name.
 
-# Now the screen is where all the magic is going to happen. Our screen will
-# have a width of 640 pixels and a height of 480 pixels. The (0,0) point will
-# be at the top left of our screen. 
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((800, 700))
 # TODO 05: Change the window size, make sure your circle code still works.
 
-# This is a loop that will run forever, simply because True is always true
 while True:
-    # Here's another loop inside of the first loop. Notice the indentation,
-    # moving one tab width into the while loop makes this statement part of the
-    # loop instead of outside of it.
     for event in pygame.event.get():
         # Let's just print all the events that happen in our window, wonder
         # what those could be?
@@ -37,19 +29,20 @@ while True:
         # Additional interactions with events
 
     # TODO 01: Make the background white by uncommenting the line below
-    # screen.fill(pygame.Color("Gray"))
+    screen.fill(pygame.Color("Gray"))
 
     # Draw things on the screen
 
     # TODO 02: Try to draw a circle (any size, any color, anywhere)
     # pygame.draw.circle(screen, color, pos, radius, width(optional)  )
+    pygame.draw.circle(screen, pygame.Color("Pink"), (400,400), 50)
 
     # TODO 03: Try to draw a red circle in the middle of the screen with a radius 100
     # pygame.draw.circle(screen, color, pos, radius, width(optional)  )
-
+    pygame.draw.circle(screen, pygame.Color("Red"), (400,250), 100)
     # TODO 04: Try to draw a yellow circle with the center exactly in the lower left corner of the screen, radius 10
     # pygame.draw.circle(screen, color, pos, radius, width(optional)  )
-
+    pygame.draw.circle(screen, pygame.Color("Blue"), (0,screen.get_height()), 100)
     # This will make sure that things appear on our screen, without this
     # update, everything we do will not be visible!
     # notice how this statement is still inside of the first while loop, but
